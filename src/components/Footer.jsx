9760f90ff9bd4ff9663438255ffc96e3b0db8462
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-10-23 10:49:53 +0100

    implement movie list

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer(){
+    return (
+        <h1>this is the component footer</h1>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1>Welcome to popcorn time!</h1>
+    </header>
+  );
+}
+
+export default Header;
